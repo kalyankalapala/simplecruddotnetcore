74e08cf497a2ffe5b872c64a9c87a314cc3f4de2
kalyankalapala <kalyankalapala24@gmail.com>
2021-08-24 12:03:26 +0530

    changes

---
+++ SampleCrudWebApplication/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -0,0 +1,21 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-fetch-data',
+  templateUrl: './fetch-data.component.html'
+})
+export class FetchDataComponent implements OnInit {
+  public forecasts: any[];
+  ngOnInit() {
+    this.mahesh()
+  }
+
+  constructor(private http: HttpClient) {
+  }
+  mahesh() {
+    this.http.get<any[]>('https://angmvcapi.azurewebsites.net/api/Employee').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
+}
---
+++ SimpleCrud/Migrations/20210824053526_kalyanh.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimpleCrud.Migrations
+{
+    public partial class kalyanh : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
